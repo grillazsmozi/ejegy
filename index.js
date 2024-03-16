7539fcbdacccda0f5ffe85c36a1ffff11390c8eb
SmartWavesCafe <smartwavescafe@gmail.com>
2024-03-16 10:35:44 +0100

    a

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const hostname = '192.168.1.115'
+
+const app = express()
+const port = 3000;
+
+// MongoDB csatlakozás
+mongoose.connect('mongodb+srv://ekreta_adminisztracio:admin@kreta-database.uozablb.mongodb.net/?retryWrites=true&w=majority&appName=kreta-database/jegyek', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('MongoDB connected successfully');
+});
+
+// Modell definíciója
+const jegySchema = new mongoose.Schema({
+    tema: String,
+    tipus: String,
+    ertek: Number,
+    tanar: String,
+    datum: String,
+    targy: String,
+    diak: String,
+});
+
+const diakSchema = new mongoose.Schema({
+    azonosito: String,
+    szuletes: String,
+    nev: String,
+})
+
+const Jegy = mongoose.model('Jegy', jegySchema);
+const Diak = mongoose.model('DiakAzonosito', diakSchema)
+
+// Middleware-ek beállítása
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Belépő oldal
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// Jegyek listázása
+app.get('/jegyek', async (req, res) => {
+    try {
+        const jegyek = await Jegy.find();
+        res.json(jegyek);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Jegy írása
+app.post('/jegyek', async (req, res) => {
+    try {
+        const { tema, tipus, tanar, ertek, datum, targy, diak } = req.body;
+        const jegy = new Jegy({ tema, tipus, ertek, tanar, datum, targy, diak });
+        await jegy.save();
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const { nev, szdatum, azonosito } = req.body
+        console.log(`${nev} ${szdatum} ${azonosito}`)
+        const diak = new Diak({ azonosito, szdatum, nev})
+        await diak.save()
+        res.redirect('/')
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+app.listen(port, hostname, () => {
+    console.log(`App listening at http://${hostname}:${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,147 @@
+function getJegyek() {
+    try {
+        fetch('/jegyek')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok, status: ${response.status}`)
+                }
+                return response.json()
+            })
+            .then(jegyek => {
+                const matek = document.getElementById('matek');
+                const magyar = document.getElementById('magyar');
+                const angol = document.getElementById('angol');
+                const term = document.getElementById('term');
+                const rajz = document.getElementById('rajz');
+                const ének = document.getElementById('ének');
+                const digi = document.getElementById('digi');
+                const tables = document.getElementById('tables');
+                const form = document.getElementById('form');
+                const diakForm = document.getElementById('diakForm');
+                console.log('Variables!')
+
+                const diakid = localStorage.getItem('diakid')
+                console.log(`Got diakid! ${diakid}`)
+
+                jegyek.forEach(jegy => {
+                    console.log('Scanning!')
+                    if (jegy.diak === diakid && diakid !== "tanar") {
+                        console.log('Passed diakid and tanar!')
+                        tables.classList.remove('hide')
+                        diakForm.classList.add('hide')
+                        form.classList.add('hide')
+                        if (jegy.targy === "matematika") {
+                            const row = matek.insertRow();
+                            const cellTema = row.insertCell(0);
+                            const cellTipus = row.insertCell(1);
+                            const cellTanar = row.insertCell(2);
+                            const cellErtek = row.insertCell(3);
+
+                            cellTema.textContent = jegy.tema;
+                            cellTipus.textContent = jegy.tipus;
+                            cellTanar.textContent = jegy.tanar;
+                            cellErtek.textContent = jegy.ertek;
+                        }
+
+                        if (jegy.targy === "magyar") {
+                            const row = magyar.insertRow();
+                            const cellTema = row.insertCell(0);
+                            const cellTipus = row.insertCell(1);
+                            const cellTanar = row.insertCell(2);
+                            const cellErtek = row.insertCell(3);
+
+                            cellTema.textContent = jegy.tema;
+                            cellTipus.textContent = jegy.tipus;
+                            cellTanar.textContent = jegy.tanar;
+                            cellErtek.textContent = jegy.ertek;
+                        }
+
+                        if (jegy.targy === "angol") {
+                            const row = angol.insertRow();
+                            const cellTema = row.insertCell(0);
+                            const cellTipus = row.insertCell(1);
+                            const cellTanar = row.insertCell(2);
+                            const cellErtek = row.insertCell(3);
+
+                            cellTema.textContent = jegy.tema;
+                            cellTipus.textContent = jegy.tipus;
+                            cellTanar.textContent = jegy.tanar;
+                            cellErtek.textContent = jegy.ertek;
+                        }
+
+                        if (jegy.targy === "term") {
+                            const row = term.insertRow();
+                            const cellTema = row.insertCell(0);
+                            const cellTipus = row.insertCell(1);
+                            const cellTanar = row.insertCell(2);
+                            const cellErtek = row.insertCell(3);
+
+                            cellTema.textContent = jegy.tema;
+                            cellTipus.textContent = jegy.tipus;
+                            cellTanar.textContent = jegy.tanar;
+                            cellErtek.textContent = jegy.ertek;
+                        }
+
+                        if (jegy.targy === "rajz") {
+                            const row = rajz.insertRow();
+                            const cellTema = row.insertCell(0);
+                            const cellTipus = row.insertCell(1);
+                            const cellTanar = row.insertCell(2);
+                            const cellErtek = row.insertCell(3);
+
+                            cellTema.textContent = jegy.tema;
+                            cellTipus.textContent = jegy.tipus;
+                            cellTanar.textContent = jegy.tanar;
+                            cellErtek.textContent = jegy.ertek;
+                        }
+
+                        if (jegy.targy === "ének") {
+                            const row = ének.insertRow();
+                            const cellTema = row.insertCell(0);
+                            const cellTipus = row.insertCell(1);
+                            const cellTanar = row.insertCell(2);
+                            const cellErtek = row.insertCell(3);
+
+                            cellTema.textContent = jegy.tema;
+                            cellTipus.textContent = jegy.tipus;
+                            cellTanar.textContent = jegy.tanar;
+                            cellErtek.textContent = jegy.ertek;
+                        }
+
+                        if (jegy.targy === "digi") {
+                            const row = digi.insertRow();
+                            const cellTema = row.insertCell(0);
+                            const cellTipus = row.insertCell(1);
+                            const cellTanar = row.insertCell(2);
+                            const cellErtek = row.insertCell(3);
+
+                            cellTema.textContent = jegy.tema;
+                            cellTipus.textContent = jegy.tipus;
+                            cellTanar.textContent = jegy.tanar;
+                            cellErtek.textContent = jegy.ertek;
+                        }
+                    } else if (localStorage.getItem('diakid') === "tanar") {
+                        tables.classList.add('hide')
+                        diakForm.classList.remove('hide')
+                        form.classList.remove('hide')
+                    }
+                });
+            })
+            .catch(error => console.error('Fetch Error:', error))
+            
+            
+    } catch (error) {
+        console.log(`Try-catch error: ${error}`)
+    }
+}
+
+function login() {
+    let loginvalue = document.getElementById('diakid').value
+
+    localStorage.setItem('diakid', loginvalue)
+    console.log(loginvalue)
+    getJegyek()
+    location.reload()
+}
+
+getJegyek()
